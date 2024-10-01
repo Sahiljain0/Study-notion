223439c242496b5c9a8693f0d4e30bd200b3f30c
Sahiljain0 <jainsahil760@gmail.com>
2024-10-01 05:37:37 -1100

    services added

--- src/Redux/reducers/index.jsx
+++ src/Redux/reducers/index.jsx
@@ -2,11 +2,15 @@
 // ******IT is the main root reducer where all the slices will be imported **
 
 import { combineReducers } from "@reduxjs/toolkit";
-import authReducer from "../slices/authSlice"
+import authReducer from "../slices/authSlice";
+import profileReducer from "../slices/profileSlice";
+import cartReducer from "../slices/cartSlice";
 const rootReducer = combineReducers({
-  reducer:{
+
     auth:authReducer,
-  }
+    profile:profileReducer,
+    cart:cartReducer,
+
 });
 
 export default rootReducer;
--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -4,8 +4,20 @@ import image from "../../assets/Logo/Logo-Full-Light.png";
 import { NavbarLinks } from "../../data/navbar-links";
 import { useLocation } from "react-router-dom";
 import { matchPath } from "react-router-dom";
-
+import { useSelector } from "react-redux";
+import { FaCartPlus } from "react-icons/fa";
 const NavBar = () => {
+
+  // fetching value of state from slices //
+
+  const {token} = useSelector((state) => state.auth);
+  const {user} = useSelector((state) => state.profile);
+  const {totalItems} = useSelector((state) => state.cart);
+
+
+
+
+  // location //
   const location = useLocation();
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname);
@@ -47,12 +59,43 @@ const NavBar = () => {
         {/* ******************************************* */}
         {/* // login, signup, dashboard buttons // */}
         <div className="flex items-center  gap-x-6 text-white">
-            <Link to="/signup">
-            <button>Signup</button>
-            </Link>
-            <Link to="/signup">
-            <button>Signup</button>
-            </Link>
+           {
+            user && user?.accountType !== "Instructor" && (
+              <Link to="/dashboard/cart" className="relative">
+                <FaCartPlus  size={24} color="yellow"/>
+                {
+                  totalItems > 0 && (
+                    <span className="text-white bg-richblack-800 p-1 text-xs flex justify-center items-center font-medium rounded-full">
+                      {totalItems}
+                    </span>
+                  )
+                }
+              </Link>
+            )
+           }
+
+           {/* // when token is null show login, signup buttons // */}
+           {
+            token === null && (
+              <Link to='/login'>
+              <button className="bg-richblack-700  p-1 px-4 font-semibold text-richblack-200 rounded-md">
+                Login</button>
+              </Link>
+            )
+           }
+            {
+            token === null && (
+              <Link to='/signup'>
+              <button className="bg-richblack-700 p-1 px-4 font-semibold text-richblack-200 rounded-md">
+              Signup</button>
+              </Link>
+            )
+           }
+           {/* // when token is having some value show profile menu */}
+           {
+            token !== null && <ProfileDropDown/>
+           }
+
         </div>
       </div>
     </div>
