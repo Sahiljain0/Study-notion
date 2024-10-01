4ed2a09c24375000dffd56b706367be187615bff
Sahiljain0 <jainsahil760@gmail.com>
2024-10-01 02:54:05 -1100

    Redux setup added

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
+import store from "../src/Redux/store/store.jsx";
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
+    
 );
