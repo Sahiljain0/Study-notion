b711d3e8cd146b48d367e913787f8ed6823509ee
Sahiljain0 <jainsahil760@gmail.com>
2024-10-05 08:36:21 -1100

    Error component added

--- src/components/core/Auth/PrivateRoute.jsx
+++ src/components/core/Auth/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({children}) => { 
 
     const {token} = useSelector((state) => state.auth);
 
