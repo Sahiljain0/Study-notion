d09e61bcc86a8b8ff58c6476068c1d80313429d8
Sahiljain0 <jainsahil760@gmail.com>
2024-10-04 13:13:30 -1100

    section 3 responsive

--- src/components/HomeSections/Section3.jsx
+++ src/components/HomeSections/Section3.jsx
@@ -5,84 +5,65 @@ import ExploreMore from "./ExploreMore";
 const Section3 = () => {
  return (
     <>
-        {/* // code section 1 */}
-        <div className="mx-20">
-        <CodeBlocks
-          position={"lg:flex-row"}
-          heading={
-            <div className="text-3xl font-semibold">
-              Unlock your
-              <HighLightText text={"coding potentials"} /> with our online
-              courses.
-            </div>
-          }
-          subheading={
-            "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate for sharing their knowledge with you"
-          }
-          ctabtn1={{
-            btntext: "Try it yourself",
-            linkto: "/signup",
-            active: true,
-          }}
-          ctabtn2={{
-            btntext: "learn more",
-            linkto: "/login",
-            active: false,
-          }}
-          codeblock={`<!Doctype html>
-                 <Head>
-                <Body> 
-                 <h1>Hello This is sahil jain....</h1>
-                 <p>Full stack developer </p>
-                 <p>hello team </p>
-                 <p>jjhu s </p>
-                <h1>hello meloni</h1>
-                 </Body>
-                 </Head>
-                 </html>`}
-          codeColor={"text-yellow-25"}
-          shadowColor={"shadow-2xl shadow-brown-400"}
-        />
-      </div>
-      {/* //code section 2// */}
-      <div className="mx-20">
-        <CodeBlocks
-          position={"lg:flex-row-reverse"}
-          heading={
-            <div className="text-3xl font-semibold">
-              Unlock your
-              <HighLightText text={"coding potentials"} /> with our online
-              courses.
-            </div>
-          }
-          subheading={
-            "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate for sharing their knowledge with you"
-          }
-          ctabtn1={{
-            btntext: "Try it yourself",
-            linkto: "/signup",
-            active: true,
-          }}
-          ctabtn2={{
-            btntext: "learn more",
-            linkto: "/login",
-            active: false,
-          }}
-          codeblock={`<!Doctype html>
-                 <Head>
-                <Body> 
-                 <h1>Hello This is sahil jain....</h1>
-                 <p>Full stack developer </p>
-                 <p>hello team </p>
-                 <p>jjhu s </p>
-                <h1>hello meloni</h1>
-                 </Body>
-                 </Head>
-                 </html>`}
-          codeColor={"text-yellow-25"}
-          shadowColor={"shadow-2xl shadow-blue-400"}
-        />
-      </div>
+      {/* Code Section 1  */}
+      <div>
+          <CodeBlocks
+            position={"lg:flex-row"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock your
+                <HighlightText text={"coding potential"} /> with our online
+                courses.
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "Try it Yourself",
+              link: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              link: "/signup",
+              active: false,
+            }}
+            codeColor={"text-yellow-25"}
+            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+            backgroundGradient={<div className="codeblock1 absolute"></div>}
+          />
+        </div>
+
+        {/* Code Section 2 */}
+        <div>
+          <CodeBlocks
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="w-[100%] text-4xl font-semibold lg:w-[50%]">
+                Start
+                <HighlightText text={"coding in seconds"} />
+              </div>
+            }
+            subheading={
+              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+            }
+            ctabtn1={{
+              btnText: "Continue Lesson",
+              link: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn More",
+              link: "/signup",
+              active: false,
+            }}
+            codeColor={"text-white"}
+            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+            backgroundGradient={<div className="codeblock2 absolute"></div>}
+          />
+        </div>
+
       <ExploreMore/>
     </>
  )
