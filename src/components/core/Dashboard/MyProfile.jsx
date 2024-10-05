219f1882529de3d297defe60cd9342ed9905afb1
Sahiljain0 <jainsahil760@gmail.com>
2024-10-05 12:33:32 -1100

    dashboard profile completed

--- src/components/core/Dashboard/MyProfile.jsx
+++ src/components/core/Dashboard/MyProfile.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import IconBtn from '../../common/IconBtn';
 import { RiEditBoxLine } from "react-icons/ri"
-
+import { formattedDate } from '../../../utils/dateFormatter';
 
 export default function MyProfile(){
     const {user} = useSelector((state) => state.profile);
@@ -61,6 +61,66 @@ export default function MyProfile(){
           {user?.additionalDetails?.about ?? "Write Something About Yourself"}
         </p>
       </div>
+      {/* ***************************************************************** */}
+      <div className="my-10 flex flex-col gap-y-10 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12">
+        <div className="flex w-full items-center justify-between">
+          <p className="text-lg font-semibold text-richblack-5">
+            Personal Details
+          </p>
+          <IconBtn
+            text="Edit"
+            onclick={() => {
+              navigate("/dashboard/settings")
+            }}
+          >
+            <RiEditBoxLine />
+          </IconBtn>
+        </div>
+        <div className="flex max-w-[500px] justify-between">
+          <div className="flex flex-col gap-y-5">
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">First Name</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.firstName}
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Email</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.email}
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Gender</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.additionalDetails?.gender ?? "Add Gender"}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Last Name</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.lastName}
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Phone Number</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {user?.additionalDetails?.contactNumber ?? "Add Contact Number"}
+              </p>
+            </div>
+            <div>
+              <p className="mb-2 text-sm text-richblack-600">Date Of Birth</p>
+              <p className="text-sm font-medium text-richblack-5">
+                {formattedDate(user?.additionalDetails?.dateOfBirth) ??
+                  "Add Date Of Birth"}
+                  {/* ?? means h to rakho ni to default value " " show krdo  */}
+              </p>
+            </div>
+          </div>
+          </div>
+          </div>
       </>
   )
 }
