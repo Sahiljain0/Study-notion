ff9e11875fc34444907179726b3fbabba8af7d03
Sahiljain0 <jainsahil760@gmail.com>
2024-10-05 05:47:09 -1100

    sidebar links logic added

--- src/components/core/Dashboard/Sidebar.jsx
+++ src/components/core/Dashboard/Sidebar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from "react";
+import { sidebarLinks } from "../../../data/dashboard-links";
+import { logout } from "../../../services/operations/authAPI";
+import { useSelector } from "react-redux";
+import SidebarLink from "./SidebarLink";
+const Sidebar = () => {
+  const { user, loading: profileLoading } = useSelector((state) => state.auth);
+  const { loading: authLoading } = useSelector((state) => state.auth);
 
- const Sidebar = () => {
+  if (authLoading || profileLoading) {
+    return (
+      <div className="grid h-[calc(100vh-3.5rem)] min-w-[220px] items-center border-r-[1px] border-r-richblack-700 bg-richblack-800">
+        <div>Loading...</div>
+      </div>
+    );
+  }
   return (
-    <div>Sidebar</div>
-  )
-}
+    <>
+      <div className="flex h-[calc(100vh-3.5rem)] min-w-[220px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800 py-10">
+        <div className="flex flex-col">
+            {
+                sidebarLinks.map((link,index) => {
+                    if(link.type && user.accountType !== link.type) return null;
+                    return(
+                        <SidebarLink link={link} iconName={link.icon}/>
+                    )
+                })
+            }
+        </div>
+      </div>
+    </>
+  );
+};
 export default Sidebar;
