623faba6f3ec1d1ce85b7b062a164c7e330faf2d
Sahiljain0 <jainsahil760@gmail.com>
2024-10-04 13:10:08 -1100

    section 1 responsive done

--- src/components/HomeSections/Section1.jsx
+++ src/components/HomeSections/Section1.jsx
@@ -6,34 +6,37 @@ import CTAButton from "../core/HomePage/Button";
 const Section1 = () => {
   return (
     <>
-      {/* // Section 1// */}
-      <Link to="/signup">
-        <div className="group p-[2px] mt-16  mx-20 rounded-full font-bold text-richblack-200  flex flex-col  bg-richblack-800  transition-all duration-500 hover:scale-95 w-fit  ">
-          <div className="rounded-full group-hover:bg-richblack-900 flex flex-row px-10 py-[5px] gap-4 items-center">
-            <p>Become an Instructor </p>
-            <FaArrowRight />
+       {/* Become a Instructor Button */}
+       <Link to={"/signup"}>
+          <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
+            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
+              <p>Become an Instructor</p>
+              <FaArrowRight />
+            </div>
           </div>
+        </Link>
+       {/* Heading */}
+       <div className="text-center text-4xl font-semibold">
+          Empower Your Future with
+          <HighlightText text={"Coding Skills"} />
         </div>
-      </Link>
-      <div className="text-4xl font-semibold text-center mt-7">
-        Empower Your Future with
-        <HighLightText text={"Coding skills"} />
-      </div>
 
-      <div className=" text-center w-[90%]  mt-4 text-lg text-richblack-300 font-bold ">
-        with our online coding courses , you can learn at your own pace , from
-        anywhere in the world and get access to a wealth of resourses. including
-        hands on projects quizes and personalized features from instructors.
-      </div>
-
-      <div className="flex flex-row gap-7 mt-8">
-        <CTAButton active={true} linkto={"/signup"}>
-          Learn More
-        </CTAButton>
-        <CTAButton active={false} linkto={"/login"}>
-          Book a Demo
-        </CTAButton>
-      </div>
+        {/* Sub Heading */}
+        <div className="-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300">
+          With our online coding courses, you can learn at your own pace, from
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
+        </div>
+       {/* CTA Buttons */}
+       <div className="mt-8 flex flex-row gap-7">
+          <CTAButton active={true} linkto={"/signup"}>
+            Learn More
+          </CTAButton>
+          <CTAButton active={false} linkto={"/login"}>
+            Book a Demo
+          </CTAButton>
+        </div>
     </>
   );
 };
