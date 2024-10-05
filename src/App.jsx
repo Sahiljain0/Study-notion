079e1b890f55593949c9f273ac873768bbe3eca1
Sahiljain0 <jainsahil760@gmail.com>
2024-10-05 08:08:24 -1100

    Profile half done

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import  ForgotPassword  from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <div className="w-screen min-h-screen flex flex-col bg-richblack-900 font-inter">
@@ -70,7 +71,6 @@ function App() {
         /> 
             <Route path="/contact" element={<Contact />} />
             <Route path="/dashboard/my-profile" element={<MyProfile/>} />
-
 
       </Routes>
     </div>
