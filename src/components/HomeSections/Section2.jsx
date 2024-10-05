54ad77f85f28f0d6f2b02df159d48fad55f0f720
Sahiljain0 <jainsahil760@gmail.com>
2024-10-04 13:11:59 -1100

    section 2 responsive

--- src/components/HomeSections/Section2.jsx
+++ src/components/HomeSections/Section2.jsx
@@ -5,12 +5,17 @@ import Banner from "../../assets/Images/banner.mp4";
 const Section2 = () => {
  return (
     <>
-      <div className="mx-3 my-12 shadow-blue-200 w-[70%] relative">
-        <div className="grad2 -top-10 w-[800px]"></div>
-        <video className="video" muted loop autoPlay>
-          <source src={Banner} type="video/mp4" />
-        </video>
-      </div>
+      {/* Video */}
+      <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
+          <video
+            className="shadow-[20px_20px_rgba(255,255,255)]"
+            muted
+            loop
+            autoPlay
+          >
+            <source src={Banner} type="video/mp4" />
+          </video>
+        </div>
     </>
  )
 }
