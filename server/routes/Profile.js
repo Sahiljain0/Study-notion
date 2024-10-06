516e31a36db084f92332923f73ab58593633778e
Sahiljain0 <jainsahil760@gmail.com>
2024-10-06 00:29:25 -1100

    new controllers routing added

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+const {auth, isInstructor} = require("../middleware/auth");
 //Profile.js //
 const {
-    updateProfile,
-    deleteAccount,
-    getAllUserDetails,
-    updateDisplayPicture,
+  deleteAccount,
+  updateProfile,
+  getAllUserDetails,
+  updateDisplayPicture,
+  getEnrolledCourses,
+  instructorDashboard,
   } = require("../controllers/Profile");
 
 // *******************************DEFINING API ROUTES******************
-
-router.put("/updateProfile", updateProfile);
-router.delete("/deleteAccount",deleteAccount);
-router.get("/getAllUserDetails",getAllUserDetails);
-router.get("/updateDisplayPicture",getAllUserDetails);
+// Delet User Account
+router.delete("/deleteProfile", auth, deleteAccount)
+router.put("/updateProfile", auth, updateProfile)
+router.get("/getUserDetails", auth, getAllUserDetails)
+// Get Enrolled Courses
+router.get("/getEnrolledCourses", auth, getEnrolledCourses)
+router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
 
 
 // ***********************************************************************
