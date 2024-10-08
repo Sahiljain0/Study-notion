580f6cd6db756ce27a1919ce9c99c309f45bcf2c
Sahiljain0 <jainsahil760@gmail.com>
2024-10-08 11:14:58 -1100

    responsive home screen

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   const { user } = useSelector((state) => state.profile)
   
   return (
-    <div className="w-screen min-h-screen flex flex-col bg-richblack-900 font-inter">
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <NavBar />
       <Toaster />
       <Routes>
