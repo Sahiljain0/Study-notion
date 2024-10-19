6a238e258c324965490838fc9e7b444935f54459
Sahiljain0 <jainsahil760@gmail.com>
2024-10-19 01:20:24 -1100

    instructore dashboard major bug fixed

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -212,33 +212,6 @@ exports.getEnrolledCourses = async (req, res) => {
   }
 }
 
-exports.instructorDashboard = async (req, res) => {
-  try {
-    const courseDetails = await Course.find({ instructor: req.user.id })
-
-    const courseData = courseDetails.map((course) => {
-      const totalStudentsEnrolled = course.studentsEnroled.length
-      const totalAmountGenerated = totalStudentsEnrolled * course.price
-
-      // Create a new object with the additional fields
-      const courseDataWithStats = {
-        _id: course._id,
-        courseName: course.courseName,
-        courseDescription: course.courseDescription,
-        // Include other course properties as needed
-        totalStudentsEnrolled,
-        totalAmountGenerated,
-      }
-
-      return courseDataWithStats
-    })
-
-    res.status(200).json({ courses: courseData })
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: "Server Error" })
-  }
-}
 
 exports.getAllUserDetails = async (req, res) => {
   try {
@@ -281,6 +254,34 @@ exports.getAllUserDetails = async (req, res) => {
 };
  
 
+
+exports.instructorDashboard = async (req, res) => {
+  try {
+    const courseDetails = await Course.find({ instructor: req.user.id })
+
+    const courseData = courseDetails.map((course) => {
+      const totalStudentsEnrolled = course.studentsEnrolled.length
+      const totalAmountGenerated = totalStudentsEnrolled * course.price
+
+      // Create a new object with the additional fields
+      const courseDataWithStats = {
+        _id: course._id,
+        courseName: course.courseName,
+        courseDescription: course.courseDescription,
+        // Include other course properties as needed
+        totalStudentsEnrolled,
+        totalAmountGenerated,
+      }
+
+      return courseDataWithStats
+    })
+
+    res.status(200).json({ courses: courseData })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Server Error h" })
+  }
+}
 // profile.js
 
 
