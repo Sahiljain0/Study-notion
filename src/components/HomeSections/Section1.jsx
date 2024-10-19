c001982db4bb623c92e884a30366a6b1fd581edf
Sahiljain0 <jainsahil760@gmail.com>
2024-10-19 08:43:17 -1100

    responsiveness

--- src/components/HomeSections/Section1.jsx
+++ src/components/HomeSections/Section1.jsx
@@ -8,15 +8,14 @@ const Section1 = () => {
     <>
        {/* Become a Instructor Button */}
        <Link to={"/signup"}>
-          <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
-            <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
-              <p>Become an Instructor</p>
-              <FaArrowRight />
+       <div className=' group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold transition-all duration-200 hover: scale-95 w-fit max-w-maxContent'>
+                <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900'>
+              <p>Become an Instructor</p><FaArrowRight/>
+                </div>
             </div>
-          </div>
         </Link>
        {/* Heading */}
-       <div className="text-center text-4xl font-semibold">
+       <div className="text-center text-3xl  md:text-4xl font-semibold">
           Empower Your Future with
           <HighLightText text={"Coding Skills"} />
         </div>
--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -46,26 +46,26 @@ const LearningGridArray = [
 
 const LearningGrid = () => {
   return (
-    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+    <div className="grid px-4 md:px-0 md:mx-auto w-full  md:w-fit grid-cols-1 md:grid-cols-4 mb-12">
       {LearningGridArray.map((card, i) => {
         return (
           <div
             key={i}
-            className={`${i === 0 && "xl:col-span-2 xl:h-[294px]"}  ${
+            className={`${i === 0 && "md:col-span-2 md:h-[294px]"}  ${
               card.order % 2 === 1
                 ? "bg-richblack-700 h-[294px]"
                 : card.order % 2 === 0
                 ? "bg-richblack-800 h-[294px]"
                 : "bg-transparent"
-            } ${card.order === 3 && "xl:col-start-2"}  `}
+            } ${card.order === 3 && "md:col-start-2"}  `}
           >
             {card.order < 0 ? (
-              <div className="xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0">
-                <div className="text-4xl font-semibold ">
+              <div className="md:w-[90%] flex flex-col gap-3 pb-10 md:pb-0">
+                <div className="md:text-4xl text-3xl font-semibold ">
                   {card.heading}
                   <HighlightText text={card.highlightText} />
                 </div>
-                <p className="text-richblack-300 font-medium">
+                <p className="text-richblack-300 font-medium ">
                   {card.description}
                 </p>
 
@@ -76,10 +76,10 @@ const LearningGrid = () => {
                 </div>
               </div>
             ) : (
-              <div className="p-8 flex flex-col gap-8">
-                <h1 className="text-richblack-5 text-lg">{card.heading}</h1>
+              <div className="lg:p-8 p-5 flex flex-col gap-4 lg:gap-8">
+                <h1 className="text-richblack-5 text-md lg:text-lg">{card.heading}</h1>
 
-                <p className="text-richblack-300 font-medium">
+                <p className="text-richblack-300 font-normal lg:font-medium">
                   {card.description}
                 </p>
               </div>
