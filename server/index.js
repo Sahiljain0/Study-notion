d1b6d22918fc382f3677a881d11499bd7d44d839
Sahiljain0 <jainsahil760@gmail.com>
2024-10-20 01:12:21 -1100

    done h

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParaser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://study-notion-woad-six.vercel.app/",
     credentials: true,
   })
 );
