bbe81fc248b14435a79aa248c4db7380155bbb8b
Sahiljain0 <jainsahil760@gmail.com>
2024-10-21 10:46:15 -1100

    test

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,7 +1,7 @@
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = {
